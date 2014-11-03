7d9990224cf430017f771d1e47cb0fa81ae467fc
Marko Idzan <midzan@tvz.hr>
2014-11-03 19:10:40 +0100

    Verzija 1.01
    i još neki sitni updatei i priprema za dalje

--- src/mojtvz/webview/MojTVZWebview.java
+++ src/mojtvz/webview/MojTVZWebview.java
@@ -5,14 +5,20 @@
  * @version 1.0 beta1
  * 
  */
+
+/**
+ * Name of package in Android settings
+ */
 package mojtvz.webview;
 
+//Importing some libraries which are needed to app work
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 
 public class MojTVZWebview extends Activity {
@@ -22,13 +28,69 @@ public class MojTVZWebview extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webview);
         final WebView pregled = (WebView)findViewById(R.id.webview);
-        pregled.loadUrl("https://moj.tvz.hr/prikaz/mojvijes");
         pregled.getSettings().setJavaScriptEnabled(true);
+        
         pregled.setWebViewClient(new WebViewClient());
-        pregled.loadUrl("javascript:document.getElementsByClassName('footer')[0].style.display = 'none';");
+        
+        
+        /*String varanjeKoda = "<html lang=\"en\">"
+        					+"<head>"
+        					+"<link rel=\"stylesheet\" href=\"http://markoidzan.me/zamjena_css_webview/zamjenjeniStil.css\">"
+        					+"</head>"
+        					+"<body>"
+        					+"</body>"
+        					+"</html>";*/
+        
+        
+
+
+          
+        
+        pregled.loadUrl("https://moj.tvz.hr/prikaz/mojvijes");
+        
+        
+        //pregled.loadData(varanjeKoda, "text/html", null);
+        
+        /*{
+        
+        	
+        	
+        	@Override
+        	public void onPageFinished(WebView view, String url)  {
+        		pregled.loadUrl("javascript:document.getElementsByTagName(img)[0].style.visibility = hidden;");
+        		
+        		
+        		
+        	}
+        	
+        	String varanjeKoda = "<html lang='en'>"+"#shadowroot";
+        	
+        	   
+        
+        
+        
+        });*/
+        
+        
+        
     }
 
+    /**
+     * If back button is pressed, app should go one link back
+     */
+    @Override
+    public void onBackPressed() {
+    	final WebView pregled = (WebView)findViewById(R.id.webview);
+        if(pregled.canGoBack()) {
+            pregled.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
 
+    /**
+     * Menu icon, ready to add some options like About and Refresh
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -41,10 +103,26 @@ public class MojTVZWebview extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        //int id = item.getItemId();
+    	//case R.id.action_refresh:
+        //Toast.makeText(this, "Refresh selected", Toast.LENGTH_SHORT)
+         //   .show();
+        //break;
+        //if (id == R.id.action_settings) {
+         //   return true;
+        //}
+        //return super.onOptionsItemSelected(item);
+    	
+    	switch (item.getItemId()) {
+        // action with ID action_refresh was selected
+        case R.id.action_settings:
+          Toast.makeText(this, "Moj TVZ verzija 1.01", Toast.LENGTH_LONG)
+              .show();
+          break;
+        default:
+          break;
         }
-        return super.onOptionsItemSelected(item);
+
+        return true;
     }
 }
