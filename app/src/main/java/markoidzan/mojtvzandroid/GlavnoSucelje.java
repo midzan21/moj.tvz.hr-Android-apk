53ff1193fd0e30653ad6fae70811825f5ebdbcdc
Marko Idzan <midzan@tvz.hr>
2015-01-21 22:46:50 +0100

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   .gitignore
    	new file:   .idea/.name
    	new file:   .idea/compiler.xml
    	new file:   .idea/copyright/profiles_settings.xml
    	new file:   .idea/encodings.xml
    	new file:   .idea/gradle.xml
    	new file:   .idea/misc.xml
    	new file:   .idea/modules.xml
    	new file:   .idea/scopes/scope_settings.xml
    	new file:   .idea/vcs.xml
    	new file:   MojTVZAndroid.iml
    	new file:   app/.gitignore
    	new file:   app/app.iml
    	new file:   app/build.gradle
    	new file:   app/proguard-rules.pro
    	new file:   app/src/androidTest/java/markoidzan/mojtvzandroid/ApplicationTest.java
    	new file:   app/src/main/AndroidManifest.xml
    	new file:   app/src/main/java/markoidzan/mojtvzandroid/GlavnoSucelje.java
    	new file:   app/src/main/res/drawable-hdpi/ic_launcher.png
    	new file:   app/src/main/res/drawable-mdpi/ic_launcher.png
    	new file:   app/src/main/res/drawable-xhdpi/ic_launcher.png
    	new file:   app/src/main/res/drawable-xxhdpi/ic_launcher.png
    	new file:   app/src/main/res/layout/activity_prikaz_weba.xml
    	new file:   app/src/main/res/menu/menu_prikaz_weba.xml
    	new file:   app/src/main/res/values-w820dp/dimens.xml
    	new file:   app/src/main/res/values/dimens.xml
    	new file:   app/src/main/res/values/strings.xml
    	new file:   app/src/main/res/values/styles.xml
    	new file:   build.gradle
    	new file:   gradle.properties
    	new file:   gradle/wrapper/gradle-wrapper.jar
    	new file:   gradle/wrapper/gradle-wrapper.properties
    	new file:   gradlew
    	new file:   gradlew.bat
    	new file:   moj.tvz.hr-Android-apk
    	new file:   settings.gradle
    
     Changes not staged for commit:
    	modified:   moj.tvz.hr-Android-apk (modified content, untracked content)

---
+++ app/src/main/java/markoidzan/mojtvzandroid/GlavnoSucelje.java
@@ -0,0 +1,85 @@
+package markoidzan.mojtvzandroid;
+
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+
+import static markoidzan.mojtvzandroid.R.id.activity_prikaz_weba;
+
+
+public class GlavnoSucelje extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prikaz_weba);
+
+        final WebView webStranica;
+
+        webStranica = (WebView) findViewById(activity_prikaz_weba);
+
+        WebSettings javascriptUkljucen = webStranica.getSettings();
+
+        javascriptUkljucen.setJavaScriptEnabled(true);
+
+        webStranica.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webStranica.loadUrl("javascript:(function(){" +
+                                    "document.getElementsByClassName('navigacija')[0].style.display='none';" +
+                                    "})()");
+                webStranica.loadUrl("javascript:(function(){" +
+                        "document.getElementsByClassName('potpis')[0].style.display='none';" +
+                        "})()");
+                webStranica.loadUrl("javascript:(function(){" +
+                        "document.getElementsByClassName('footer')[0].style.display='none';" +
+                        "})()");
+                webStranica.loadUrl("javascript:(function(){" +
+                        "document.getElementsByTagName('img')[0].style.display='none';" +
+                        "})()");
+                webStranica.loadUrl("javascript:(function(){" +
+                        "document.getElementsByTagName('body')[0].background='#fff';" +
+                        "})()");
+
+            }
+        });
+
+        webStranica.loadUrl("https://moj.tvz.hr/prikaz/mojvijes");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_prikaz_weba, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
